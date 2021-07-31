f501dbdc5b3b3cdec0844ad4af2e2a1544fd6977
yamanoku <0910yama@gmail.com>
2021-07-31 21:29:15 +0900

    add: VisuallyHidden

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>App</div>
+  <div>
+    <VisuallyHidden>
+      example
+    </VisuallyHidden>
+    <VisuallyHidden :isFocus="true">
+      <a href="http://example.com">example</a>
+    </VisuallyHidden>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import VisuallyHidden from "./VisuallyHidden.vue";
 export default defineComponent({
-  name: "App"
+  name: "App",
+  components: {
+    VisuallyHidden
+  }
 });
 </script>
---
+++ src/VisuallyHidden.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="focusedClass">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "VisuallyHidden",
+  props: {
+    isFocus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const focusedClass = props.isFocus
+      ? "visually-hidden-focusable"
+      : "visually-hidden";
+    return {
+      focusedClass
+    }
+  },
+});
+</script>
+
+<style scoped>
+/* https://github.com/ampproject/amphtml/blob/2e9a940e78df8de38a6c06a0772aeaa4839a24d1/css/ampshared.css#L164-L194 */
+.visually-hidden,
+.visually-hidden-focusable:not(:focus):not(:focus-within) {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 4px;
+  height: 4px;
+  opacity: 0;
+  overflow: hidden;
+  border: none;
+  margin: 0;
+  padding: 0;
+  display: block;
+  visibility: visible;
+}
+</style>
